1c27c5e5a441445e4c3e7e1d4b58a4efe122485b
Tetiana-V-Kovalenko <113469589+Tetiana-V-Kovalenko@users.noreply.github.com>
2022-11-29 21:12:26 +0200

    trending pagination

--- src/js/trends.js
+++ src/js/trends.js
@@ -1,31 +1,81 @@
 const axios = require('axios').default;
-
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 import cardHTML from '../tamlates/gallery-card.hbs';
 
 const gallery = document.querySelector('.gallery');
-gallery.addEventListener('click', onClickGallery);
+document.addEventListener('DOMContentLoaded', contentLoaded);
+const container = document.getElementById('tui-pagination-container');
 
-async function onClickGallery() {
+const options = {
+  totalItems: 20000,
+  itemsPerPage: 20,
+  visiblePages: 10,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  // template: {
+  //   page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+  //   currentPage:
+  //     '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+  //   moveButton:
+  //     '<a href="#" class="tui-page-btn tui-{{type}}">' +
+  //     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+  //     '</a>',
+  //   disabledMoveButton:
+  //     '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+  //     '<span class="tui-ico-{{type}}">{{type}}</span>' +
+  //     '</span>',
+  //   moreButton:
+  //     '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+  //     '<span class="tui-ico-ellip">...</span>' +
+  //     '</a>',
+  // },
+};
+
+const myPagination = new Pagination(container, options);
+myPagination.on('afterMove', eventData => {
+  options.page = eventData.page;
+  contentLoaded();
+});
+
+async function contentLoaded() {
   gallery.innerHTML = '';
   const arrCards = await fetchMovieCard();
+  const arrOfGenres = await fetchGenreIds();
   console.log(arrCards);
-  console.log(arrCards.results);
+  sessionStorage.setItem('genres', JSON.stringify(arrOfGenres));
   appendCardMarkup(arrCards);
 }
 
 async function fetchMovieCard() {
   const res = await axios.get(
-    'https://api.themoviedb.org/3/trending/movie/week?api_key=9cda16d98a6e510af2decf0d66e8e7d5'
+    `https://api.themoviedb.org/3/trending/movie/week?api_key=9cda16d98a6e510af2decf0d66e8e7d5&page=${options.page}`
   );
   return res.data;
 }
+async function fetchGenreIds() {
+  const arrGenres = await axios.get(
+    'https://api.themoviedb.org/3/genre/movie/list?api_key=9cda16d98a6e510af2decf0d66e8e7d5'
+  );
 
+  return arrGenres.data.genres;
+}
 function appendCardMarkup(arrCards) {
-  //   let galleryCardsHTML = '';
-  //   gallery.innerHTML = cardHTML(arrCards.results);
-  //   arrCards.results.map(
-  //       ({ title, release_date, poster_path, id }) =>
+  const newArrCard = arrCards.results.map(result => {
+    return {
+      ...result,
+      release_date: result.release_date.slice(0, 4),
+      genre_names: result.genre_ids.map(id => definitionGenre(id)).join(', '),
+    };
+  });
+  console.log(newArrCard);
+  gallery.insertAdjacentHTML('beforeend', cardHTML(newArrCard));
+}
+function definitionGenre(id) {
+  const listAllGenres = JSON.parse(sessionStorage.getItem('genres'));
+  const searchedGenre = listAllGenres.find(genre => genre.id === id);
 
-  //     );
-  gallery.insertAdjacentHTML('beforeend', cardHTML(arrCards.results));
+  return searchedGenre.name;
 }
