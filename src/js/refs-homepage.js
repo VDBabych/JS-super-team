5ba052e8a053eceabeea45754e57a4d44297597b
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2022-11-30 13:28:30 +0200

    Fix bugs in header mobile
    And add picture for 0 results of search

--- src/js/refs-homepage.js
+++ src/js/refs-homepage.js
@@ -4,4 +4,5 @@ export const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   container: document.getElementById('tui-pagination-container'),
+  notifySearchFailure: document.querySelector('.search-notify-failure')
 }
