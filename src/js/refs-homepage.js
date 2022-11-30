c891df6e0b8bcb983f64db3cd2616d078528e55e
Volodymyr Babych <v.d.babych@gmail.com>
2022-11-30 14:32:26 +0200

    solve conflict

--- src/js/refs-homepage.js
+++ src/js/refs-homepage.js
@@ -4,6 +4,6 @@ export const refs = {
   searchForm: document.querySelector('.search-form'),
   gallery: document.querySelector('.gallery'),
   container: document.getElementById('tui-pagination-container'),
-  notifySearchFailure: document.querySelector('.search-notify-failure')
+  notifySearchFailure: document.querySelector('.search-notify-failure'),
   spinner: document.querySelector('.lds-roller'),
 };
