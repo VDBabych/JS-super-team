336f70b44cc8d1a58e5ab9a56b33304139cda2af
Semenov Ihor <Semenovwork27@gmail.com>
2022-11-30 14:38:15 +0200

    1

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -4,22 +4,40 @@ const STORAGE_QUEUE = 'queue-state';
 const refs = {
     libraryWatched: document.querySelector('.header-library__btn.watched'),
     libraryQueue: document.querySelector('.header-library__btn.queue'),
-    addLibraryWatched: document.querySelector('.btn-add-watched'),
-    addLibraryQueue: document.querySelector('.btn-add-queue')
+    cardId: document.querySelector('modal__img')
+    // addLibraryWatched: document.querySelector('.btn-add-watched'),
+    // addLibraryQueue: document.querySelector('.btn-add-queue')
 };
 
-console.log(refs.addLibraryWatched);
 
-let selectedWatched = [];
+
 let selectedQueue = [];
+
+// addSelectedQueue
+
+export function addSelectedWatched(e) {
+    let selectedWatched = [];
+    selectedWatched.push(e)
+    localStorage.setItem(STORAGE_WATCHED, JSON.stringify(selectedWatched));
+}
+
+export function addSelectedQueue(e) {
+    let selectedWatched = [];
+    selectedWatched.push(e)
+    localStorage.setItem(STORAGE_WATCHED, JSON.stringify(selectedWatched));
+}
+
+function getSelectedWatched(e) {
+   let StorageItem =  JSON.parse(localStorage.getItem(STORAGE_WATCHED));
+}
 
 // initForm();
 
-refs.addLibraryWatched.addEventListener('click', () => {
-    // const selectedId = ;
+// refs.addLibraryWatched.addEventListener('click', () => {
+//     // const selectedId = ;
 
-    localStorage.setItem(STORAGE_WATCHED, JSON.stringify(selectedId));
-});
+//     localStorage.setItem(STORAGE_WATCHED, JSON.stringify(selectedId));
+// });
 
 
 // filterForm.addEventListener('click', onFormSubmit);
