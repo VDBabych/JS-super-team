dc264156522e544d1c873715c97fc2be490070b7
Tetiana-V-Kovalenko <113469589+Tetiana-V-Kovalenko@users.noreply.github.com>
2022-12-01 14:49:12 +0200

    tui pagination styles

--- src/js/trends.js
+++ src/js/trends.js
@@ -12,11 +12,28 @@ document.addEventListener('DOMContentLoaded', contentLoad);
 export const options = {
   totalItems: 20000,
   itemsPerPage: 20,
-  visiblePages: 10,
+  visiblePages: 5,
   page: 1,
   centerAlign: true,
-  firstItemClassName: 'tui-first-child',
-  lastItemClassName: 'tui-last-child',
+  firstItemClassName: 'main-first-child',
+  lastItemClassName: 'main-last-child',
+  template: {
+    page: '<a href="#" class="  main-page-pag-btn">{{page}}</a>',
+    currentPage:
+      '<strong class=" main-page-pag-btn main-page-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="#" class=" main-page-pag-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class=" main-page-pag-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="#" class=" main-page-pag-btn main-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
 };
 
 export const trendPagination = new Pagination(refs.container, options);
