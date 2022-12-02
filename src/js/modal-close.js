cf5acc88cca08a4a8d2b70eef49d900e56e7edb4
Tetiana-V-Kovalenko <113469589+Tetiana-V-Kovalenko@users.noreply.github.com>
2022-12-02 21:47:24 +0200

    trailer

--- src/js/modal-close.js
+++ src/js/modal-close.js
@@ -1,8 +1,9 @@
 'use strict';
+import axios from '../../node_modules/axios/index';
+import * as basicLightbox from 'basiclightbox';
 import createModalMurkupById from '../tamlates/modal.hbs';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { addSelectedWatched } from './local-storage';
-import { addSelectedQueue } from './local-storage';
+import { addSelectedWatched, addSelectedQueue } from './local-storage';
 import { initId } from './check-include-id';
 import { MovieAPI } from './movie-API';
 
@@ -12,12 +13,14 @@ const refs = {
   btnAddWatched: null,
   btnAddQueue: null,
   btnCloseEl: null,
+  // btnTrailer: null,
   backdropEl: document.querySelector('.backdrop'),
   galleryEl: document.querySelector('.gallery'),
 };
 
 let cardsElems = [];
 let index = null;
+let modalTrailer;
 
 function updateDataForModal(data) {
   return {
@@ -45,7 +48,10 @@ async function onGalleryClick(e) {
   try {
     const data = await movieApi.getMovieById(idMovie);
     const propertieMovie = updateDataForModal(data);
-    refs.backdropEl.insertAdjacentHTML('beforeend', createModalMurkupById(propertieMovie));
+    refs.backdropEl.insertAdjacentHTML(
+      'beforeend',
+      createModalMurkupById(propertieMovie)
+    );
   } catch (error) {
     Notify.failure(error.message);
     return;
@@ -56,20 +62,19 @@ async function onGalleryClick(e) {
 
   initId();
   cardsElems = document.querySelectorAll('.gallery_card');
-    
-  index = [...cardsElems].findIndex((itemLi) => {
+
+  index = [...cardsElems].findIndex(itemLi => {
     return itemLi === item;
   });
 }
 
 refs.backdropEl.addEventListener('click', onBackdropClick);
 
-function onBackdropClick(e) {
-  
-  
-
+async function onBackdropClick(e) {
+  console.log(e.target);
   if (e.target.classList.contains('backdrop')) {
     console.log('backdrop close');
+
     closeModal();
   }
 
@@ -82,9 +87,25 @@ function onBackdropClick(e) {
     console.log('btn-queue');
     addSelectedQueue();
   }
-  
+  if (e.target.classList.contains('btn-trailer')) {
+    console.log(e.target);
+
+    await onBtnTrailer();
+    console.log(e.target);
+    console.log(document.querySelector('.basicLightbox__placeholder'));
+    document
+      .querySelector('.basicLightbox__placeholder')
+      .addEventListener('click', e => {
+        console.log(e.target);
+        if (e.target.classList.contains('basicLightbox__placeholder')) {
+          modalTrailer.close();
+        }
+      });
+  }
+
   if (e.target.closest('.modal__btn-close')) {
     console.log('close btn');
+
     closeModal();
   }
 
@@ -97,24 +118,29 @@ function onBackdropClick(e) {
     onBtnMinus();
   }
 }
+async function onBtnTrailer() {
+  const imgSelected = document.querySelector('.modal__img');
+  const idMovie = imgSelected.dataset.id;
 
- function btnPlus() {
-    index += 1;
-    console.log(index);
-    if (index > cardsElems.length - 1) {
-        index = 0;
-    }
-   
-    const nextIdOfElements = cardsElems[index].dataset.id;
-        
-    getFetchCardById(nextIdOfElements);
+  await fetchAndCreateTrailer(idMovie);
+}
+function btnPlus() {
+  index += 1;
+  console.log(index);
+  if (index > cardsElems.length - 1) {
+    index = 0;
+  }
+
+  const nextIdOfElements = cardsElems[index].dataset.id;
+
+  getFetchCardById(nextIdOfElements);
 }
 
 function onBtnMinus() {
   index -= 1;
   console.log(index);
   if (index < 0) {
-      index = cardsElems.length - 1;
+    index = cardsElems.length - 1;
   }
 
   const nextIdOfElements = cardsElems[index].dataset.id;
@@ -124,6 +150,7 @@ function onBtnMinus() {
 
 function closeModal() {
   console.log('im close');
+  modalTrailer.close();
   refs.backdropEl.querySelector('.modal').remove();
   document.body.style.paddingRight = '0';
   document.removeEventListener('keydown', onEscDown);
@@ -133,6 +160,7 @@ function closeModal() {
 
 function onEscDown(e) {
   if (e.code === 'Escape') {
+    modalTrailer.close();
     closeModal();
   }
 }
@@ -141,16 +169,29 @@ async function getFetchCardById(id) {
   try {
     const data = await movieApi.getMovieById(id);
     const propertieMovie = updateDataForModal(data);
-    
+
     refs.backdropEl.querySelector('.modal').remove();
-    refs.backdropEl.insertAdjacentHTML('beforeend', createModalMurkupById(propertieMovie));
+    refs.backdropEl.insertAdjacentHTML(
+      'beforeend',
+      createModalMurkupById(propertieMovie)
+    );
     initId();
-    
   } catch (error) {
     Notify.failure(error.message);
     return;
   }
-  
 }
 
+async function fetchAndCreateTrailer(id) {
+  const responseWithVideo = await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}/videos?api_key=9cda16d98a6e510af2decf0d66e8e7d5`
+  );
+
+  modalTrailer = basicLightbox.create(
+    `
+  <iframe class='iframe-trailer' width="560" height="315" src="https://www.youtube.com/embed/${responseWithVideo.data.results[0].key}" frameborder="0" allowfullscreen></iframe>
+`
+  );
+  modalTrailer.show();
+}
 refs.galleryEl.addEventListener('click', onGalleryClick);
