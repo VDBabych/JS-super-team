2a4ededb25b803b15fd7b045e49fa24a09cf9261
Olha Prymak <113449585+peckeby@users.noreply.github.com>
2022-12-04 00:09:04 +0200

    Make pictures for empty pages and sections

--- src/js/library/queue-btn.js
+++ src/js/library/queue-btn.js
@@ -4,18 +4,20 @@ import { getQueue } from '../init-form';
 import { MovieAPI } from '../movie-API';
 import { appendCardMarkupLibrary } from './append-card-markup-library';
 const movieApi = new MovieAPI();
-const STORAGE_QUEUE = 'queue-state';
+// const STORAGE_QUEUE = 'queue-state';
 
 export async function onQueueClick() {
   refs.gallery_library.innerHTML = '';
-
+  refs.library_empty.classList.remove("library_empty", "watched_empty");
   refs.spinner.classList.remove('hidden');
   const arrSelectedQueue = getQueue();
+  if (arrSelectedQueue.length === 0){
+    refs.library_empty.classList.add("queue_empty")
+  }
   const arrFetchPromises = arrSelectedQueue.map(el =>
     movieApi.getMovieById(el)
   );
   const responseOfFetchPromises = await Promise.all(arrFetchPromises);
-
   appendCardMarkupLibrary(responseOfFetchPromises);
   refs.spinner.classList.add('hidden');
 }
